2dcd0a5f09eed61e255fa34fcdbcc0be5d495ef1
adeoluwagbemiga <adeoluwagbemiga@gmail.com>
2017-08-28 13:28:56 +0100

    Initial commit

---
+++ app/src/main/java/com/adeoluwa/android/parcelablesample/Organization.java
@@ -0,0 +1,14 @@
+package com.adeoluwa.android.parcelablesample;
+
+/**
+ * Created by Merlyne on 8/28/2017.
+ */
+
+public class Organization {
+    private int id;
+    private String name;
+    private byte[] logo;
+    private float worth;
+    private double noOfEmployees;
+    private String address;
+}
